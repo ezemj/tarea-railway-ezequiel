af09aeaf2ceb646740db371083a85ee223160754
ezemj <eze.maja@gmail.com>
2026-02-10 20:44:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h1>Tarea RA6 - Despliegue en Railway</h1>";
 echo "<p>Alumno: Ezequiel Martínez Jara</p>";
-echo "<p>Estado: Versión 1.0</p>";
+echo "<p>Estado: Versión 2.0 (Actualizado automáticamente)</p>";
 ?>
